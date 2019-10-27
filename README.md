76d9764e64f40682d36dd4d96bc3808c410eb1b0
Kurt Tometich <boomtown@Kurts-MacBook-Pro.local>
2019-10-27 13:11:56 -0600

    updating readme and docker commands to use 4200

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Docker Image Build and Container Run 
+
+Run ./startApp.sh to build and start the docker container.  App available at localhost:4200
+
+## Stop Docker Container
+
+Run ./stopApp.sh to stop the currently running docker container.  
+
+## Deploy the Docker container to ECR
+
+TBD... 
--- startApp.sh
+++ startApp.sh
@@ -6,10 +6,10 @@ docker build -t example:dev .
 
 # run the docker image and mount at ./app and remove container and volumes on container exit
 echo "Starting angular app within Docker container."
-containerId=$(docker run -d -v ${PWD}:/app -v /app/node_modules -p 4201:4200 --name app --rm example:dev)
+containerId=$(docker run -d -v ${PWD}:/app -v /app/node_modules -p 4200:4200 --name app --rm example:dev)
 echo "Started container with id:$containerId"
 
 # store the container id locally to be able to stop it later
 echo $containerId > .runningContainer
 sleep 5
-echo "Check http://localhost:4201"
+echo "Check http://localhost:4200"
